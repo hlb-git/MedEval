bd67baa84cb53f32705dc063fa070c4d9bdebe3b
hlb-git <mzeelovegeneral@gmail.com>
2024-12-27 16:14:56 +0100

    fix(css): fixed asset location bugs

--- frontend/frontend-medeval/src/components/body/body.tsx
+++ frontend/frontend-medeval/src/components/body/body.tsx
@@ -1,5 +1,5 @@
 import "./body.css";
-import review from "../../assets/review.jpg";
+import review from "../../assets/telemed.jpg";
 import google from "../../assets/google.png";
 import apple from "../../assets/apple.png";
 
@@ -10,13 +10,14 @@ function Body() {
         <div className="start-health">
           <div className="left">
             <h2>Start Your Health Journey Now!</h2>
+            <p>Consult a medical practiotional at the comfort of your home</p>
             <div className="icons">
               <img src={google} alt="" className="google" />
               <img src={apple} alt="" className="apple" />
             </div>
           </div>
           <div className="right">
-            <img src={review} alt="" className="review" />
+            <img src={review} alt="" className="telemed" />
           </div>
         </div>
       </div>
