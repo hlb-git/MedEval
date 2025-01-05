b592088a4595de802e739fa92b316fd01f3ffce2
hlb-git <mzeelovegeneral@gmail.com>
2025-01-05 18:26:37 +0100

    feat: implemented routes and svg icons to style dashboard menu

--- frontend/frontend-medeval/src/components/sidebar/sidebar.tsx
+++ frontend/frontend-medeval/src/components/sidebar/sidebar.tsx
@@ -1,25 +1,66 @@
 import "./sidebar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTachometerAlt,
+  faCalendarCheck,
+  faFileMedical,
+  faVideo,
+  faFilePrescription,
+  faBell,
+  faCog,
+  faQuestionCircle,
+  faSignOutAlt, 
+} from "@fortawesome/free-solid-svg-icons";
 
 function Sidebar() {
-    return (
-        <div className="sbar_wrapper">
-            <div className="sbar_container">
-                <ul className="sbar_menu">
-                    <li> Dashboard </li>
-                    <li key="Appointments"> Appointments</li>
-                    <li key="Medical Records"> Medical Records</li>
-                    <li key="Teleconsultations"> Teleconsultations</li>
-                    <li key="Prescriptions"> Prescriptions</li>
-                    <li key="Notifications"> Notifications</li>
-                </ul>
-                <ul className="controls">
-                    <li key="Settings"> Settings</li>
-                    <li key="Support"> Support</li>
-                    <li key="Logout"> Logout</li>
-                </ul>
-            </div>
-        </div>
-    )
+  return (
+    <div className="sbar_wrapper">
+      <div className="sbar_container">
+        <span className="welcome"> Welcome, [firstname] </span>
+        <span className="email"> [email] </span>
+        <br></br>
+        <ul className="sbar_menu">
+        <li>
+            <FontAwesomeIcon icon={faTachometerAlt} className="icon" />
+            Dashboard
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faCalendarCheck} className="icon" />
+            Appointments
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faFileMedical} className="icon" />
+            Medical Records
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faVideo} className="icon" />
+            Teleconsultations
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faFilePrescription} className="icon" />
+            Prescriptions
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faBell} className="icon" />
+            Notifications
+          </li>
+        </ul>
+        <ul className="controls">
+          <li>
+            <FontAwesomeIcon icon={faCog} className="icon" />
+            Settings
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faQuestionCircle} className="icon" />
+            Support
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faSignOutAlt} className="icon" />
+            Logout
+          </li>
+          </ul>
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar;
