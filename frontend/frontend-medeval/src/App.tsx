095a10a7d9179a6ab135cee0af24441697d1caaf
hlb-git <mzeelovegeneral@gmail.com>
2025-01-07 17:03:52 +0100

    feat: created auth page

--- frontend/frontend-medeval/src/App.tsx
+++ frontend/frontend-medeval/src/App.tsx
@@ -1,22 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Auth from "./pages/auth";
 import "./App.css";
 import Home from "./pages/home";
 import Dashboard from "./pages/dashboard";
 
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={
-                    <div className="app">
-                        <Home />
-                    </div>} />
-                <Route path="/dashboard" element={<Dashboard />}/>
-                </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <div className="app">
+            <Home />
+          </div>} />
+        <Route path="/dashboard" element={
+          <Dashboard />}/>
+        <Route path="/auth" element={
+          <Auth />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
