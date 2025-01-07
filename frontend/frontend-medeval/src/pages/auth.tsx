3eac14211663d8e9d131dff4100d0748ed93dc3e
hlb-git <mzeelovegeneral@gmail.com>
2025-01-07 22:16:12 +0100

    feat: impleted logging and signup toggle button on auth page

--- frontend/frontend-medeval/src/pages/auth.tsx
+++ frontend/frontend-medeval/src/pages/auth.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import illustration from '../assets/illustration.jpg';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import {
@@ -23,38 +24,68 @@ function Auth() {
   return (
     <MDBContainer fluid>
 
-      <MDBCard className='text-black m-5' style={{borderRadius: '25px', background: "#F1F0EE"}}>
+      <MDBCard className='text-black m-5' style={{
+        borderRadius: '25px', background: "#F1F0EE", }}>
         <MDBCardBody>
           <MDBRow>
-            <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center' style={{height: '75vh', background: "#F1F0EE"}}>
+            <MDBCol md='10' lg='6' 
+            className='order-2 order-lg-1 d-flex flex-column 
+            justify-content-center align-items-center' 
+            style={{height: '75vh', background: "#F1F0EE"}}>
 
-              <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">{isSignup ? "Sign Up" : "Login"}</p>
+              <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">{
+                isSignup ? "Sign Up" : "Login"}</p>
 
-              <div className="d-flex flex-row align-items-center mb-4">
-                <MDBIcon fas icon="envelope me-3" size='lg'/>
-                <MDBInput label='Your Email' id='form2' type='email'/>
-              </div>
+              {isSignup ? (
+                <>
+                  <div className="d-flex flex-row align-items-center mb-4">
+                    <MDBIcon fas icon="envelope me-3" size='lg'/>
+                    <MDBInput label='Your Email' id='form2' type='email'/>
+                  </div>
+    
+                  <div className="d-flex flex-row align-items-center mb-4">
+                    <MDBIcon fas icon="lock me-3" size='lg'/>
+                    <MDBInput label='Password' id='form3' type='password'/>
+                  </div>
+    
+                  <div className="d-flex flex-row align-items-center mb-4">
+                    <MDBIcon fas icon="key me-3" size='lg'/>
+                    <MDBInput label='Repeat your password' id='form4' type='password'/>
+                  </div> 
+                  <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' 
+                  label='Subscribe to our newsletter' />
 
-              <div className="d-flex flex-row align-items-center mb-4">
-                <MDBIcon fas icon="lock me-3" size='lg'/>
-                <MDBInput label='Password' id='form3' type='password'/>
-              </div>
-
-              <div className="d-flex flex-row align-items-center mb-4">
-                <MDBIcon fas icon="key me-3" size='lg'/>
-                <MDBInput label='Repeat your password' id='form4' type='password'/>
-              </div>
+              </>
+              ) : (
+                <>
+                   <div className="d-flex flex-row align-items-center mb-4">
+                    <MDBIcon fas icon="envelope me-3" size='lg'/>
+                    <MDBInput label='Your Email' id='form2' type='email'/>
+                  </div>
+    
+                  <div className="d-flex flex-row align-items-center mb-4">
+                    <MDBIcon fas icon="lock me-3" size='lg'/>
+                    <MDBInput label='Password' id='form3' type='password'/>
+                  </div>
+                </>
+              )}
 
               <div className='mb-4'>
-                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Subscribe to our newsletter' />
               </div>
+              
+              <p className="small text-muted">
+                {isSignup ? "Already have an account?" : "Don't have an account?"}
+                <a href="#!" onClick={toggleView} className="text-primary ms-1">
+                  {isSignup ? "Log in" : "Sign up"}
+                </a>
+              </p>
 
               <MDBBtn className='mb-4' size='lg'>Register</MDBBtn>
 
             </MDBCol>
 
             <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
-              <MDBCardImage style={{borderRadius: "20px"}} src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp' fluid/>
+              <MDBCardImage style={{borderRadius: "20px"}} src={illustration} fluid/>
             </MDBCol>
 
           </MDBRow>
